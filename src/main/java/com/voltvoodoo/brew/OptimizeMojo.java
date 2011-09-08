9f6be85c088bee59dc5c521b4dfe7ef508f2281f
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-09-08 13:20:29 +0200

    removed system.out.println

--- src/main/java/com/voltvoodoo/brew/OptimizeMojo.java
+++ src/main/java/com/voltvoodoo/brew/OptimizeMojo.java
@@ -415,8 +415,6 @@ public class OptimizeMojo extends AbstractMojo {
         ObjectMapper mapper = new ObjectMapper();
         mapper.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
         mapper.writeValue( profileFile, this );
-        System.out.println("----------------");
-        mapper.writeValue(System.out, this);
         return profileFile;
     }
 
