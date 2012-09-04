9178a9df97d2ef6528d1ef531a9959330ed96699
Abhishek Reddy <abhishek.reddy@smxemail.com>
2012-09-05 11:02:58 +1200

    Added minimal functional test for includeRequire.

---
+++ src/functionaltest/testprojects/includeRequireTest/src/main/javascript/main.js
@@ -0,0 +1,10 @@
+/**
+ * A comment.
+ */
+require(["one", "two"], function (one, two) {
+    // An inline comment
+    
+    /*
+     * And a multiline inline comment.
+     */
+});
---
+++ src/functionaltest/testprojects/includeRequireTest/src/main/javascript/two.js
@@ -0,0 +1,8 @@
+define(['order!three'],function() {
+    
+    var thisWontBeHereIfThisFileIsMinified = true;
+
+    return {
+        'akeythatwecanlookfor' : "two.js"
+    };
+});
